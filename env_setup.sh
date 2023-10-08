8db6f45e4e8990d2fd82617942cd5cbafa4e13a6
rigel0586 <rigel0586@gmail.com>
2023-10-08 13:45:32 +0300

    Update genie env var

--- env_setup.sh
+++ env_setup.sh
@@ -16,7 +16,8 @@ googletest_dir=${base_directory}/${third_party_dir}/googletest
 
 cd ${fw_directory}
 
-export GENIE=${fw_directory}/genie/install
+export GENIE=${fw_directory}/genie/build/genie
+GENIE_install=${fw_directory}/genie/install
 export ROOTSYS=${fw_directory}/cern_root/install
 export PATH=$PATH:${fw_directory}/cern_root/install/bin
 export PYTHIA6=${dependencies_dir}/genie/pythia6/v6_428/lib/
@@ -27,9 +28,9 @@ export LHAPDF6_LIB=${dependencies_dir}/genie/LHAPDF-6.5.4/install/lib/
 export GEANT4_INC=${fw_directory}/geant4/install/include/
 export GEANT4_LIB=${fw_directory}/geant4/install/lib/ 
 
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${fw_directory}/geant4/install/lib/:${dependencies_dir}/genie/lhapdf-5.9.1/install/lib/:${dependencies_dir}/genie/log4cpp/install/lib/:${dependencies_dir}/geant4/2.4.6.4/install/lib/:${dependencies_dir}/genie/log4cpp/install/lib/:${LHAPDF6_LIB}:${LHAPDF5_LIB}/.libs:/usr/local/lib:${GENIE}/lib:${ROOTSYS}/lib:${PYTHIA6}
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${fw_directory}/geant4/install/lib/:${dependencies_dir}/genie/lhapdf-5.9.1/install/lib/:${dependencies_dir}/genie/log4cpp/install/lib/:${dependencies_dir}/geant4/2.4.6.4/install/lib/:${dependencies_dir}/genie/log4cpp/install/lib/:${LHAPDF6_LIB}:${LHAPDF5_LIB}/.libs:/usr/local/lib:${GENIE_install}/lib:${ROOTSYS}/lib:${PYTHIA6}
 
-export PATH=$PATH:${ROOTSYS}/bin:${GENIE}/bin 
+export PATH=$PATH:${ROOTSYS}/bin:${GENIE_install}/bin 
 
 
 
