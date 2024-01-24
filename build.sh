1a7f1764e16e2657ebbe8b21de403950570addf1
rigel0586 <rigel0586@gmail.com>
2024-01-25 01:17:33 +0200

    Add fairlogger

--- build.sh
+++ build.sh
@@ -24,6 +24,7 @@ geant4_dir=${base_directory}/${third_party_dir}/geant4_11_1
 genfit_dir=${base_directory}/${third_party_dir}/genfit
 genie_dir=${base_directory}/${third_party_dir}/genie
 googletest_dir=${base_directory}/${third_party_dir}/googletest
+fairlogger_dir=${base_directory}/${third_party_dir}/fairlogger
 framework_dir=${base_directory}/framework
 
 cd ${fw_directory}
@@ -160,6 +161,23 @@ mkdir log4cpp
 cd ${fw_directory}
 cp -R ${dependencies_dir}/genie/log4cpp/install/lib ./log4cpp
 cp -R ${dependencies_dir}/genie/log4cpp/install/include ./log4cpp
+
+cd ${fw_directory}
+
+# Fairlogger
+mkdir fairlogger
+cd fairlogger
+mkdir build
+mkdir install
+cd build
+cmake -S ${fairlogger_dir} -B . -DCMAKE_INSTALL_PREFIX=${fw_directory}/fairlogger/install
+cmake --build . 
+cmake --install .
+
+fairlogger_lib=${fw_directory}/fairlogger/install/lib/libFairLogger.so
+check_lib_exists ${fairlogger_lib}
+
+cd ${fw_directory}
 
 # Framework Essnusb v3
 cd ${framework_dir}
