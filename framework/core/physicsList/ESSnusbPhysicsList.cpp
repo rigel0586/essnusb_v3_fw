be65677bd3a0f7da2774e2cd5de1b44cc8900fff
rigel0586 <rigel0586@gmail.com>
2024-01-29 07:03:44 +0200

    Add physics list

--- framework/core/physicsList/ESSnusbPhysicsList.cpp
+++ framework/core/physicsList/ESSnusbPhysicsList.cpp
@@ -11,40 +11,53 @@ namespace esbroot {
 namespace esbcore {
 namespace physicslist {
 
-ESSnusbPhysicsList::ESSnusbPhysicsList() {}
+ESSnusbPhysicsList::ESSnusbPhysicsList() : G4VModularPhysicsList()
+{
+    G4int ver = 1;
+    SetVerboseLevel(ver);
+
+    // EM Physics
+    RegisterPhysics( new G4EmStandardPhysics(ver) );
+    RegisterPhysics( new G4EmLivermorePhysics(ver) );
+    RegisterPhysics( new G4EmPenelopePhysics(ver) );
+    RegisterPhysics( new G4EmLowEPPhysics(ver) );
+
+    // Synchroton Radiation & GN Physics
+    RegisterPhysics( new G4EmExtraPhysics(ver) );
+
+    // Decays
+    RegisterPhysics( new G4DecayPhysics(ver) );
+
+    // Ions
+    RegisterPhysics( new G4IonPhysics(ver) );
+
+    // Hadron physics
+    RegisterPhysics( new G4HadronElasticPhysics(ver) );
+    
+    // Optical
+    RegisterPhysics( new G4OpticalPhysics(ver));
+
+    // Other
+    RegisterPhysics( new G4StoppingPhysics(ver) );
+}
 
 ESSnusbPhysicsList::~ESSnusbPhysicsList() {}
 
 void ESSnusbPhysicsList::ConstructParticle() {
-  // Construct all bosons
-  G4BosonConstructor bosons;
-  bosons.ConstructParticle();
-
-  // Construct all leptons
-  G4LeptonConstructor leptons;
-  leptons.ConstructParticle();
-
-  // Construct all mesons
-  G4MesonConstructor mesons;
-  mesons.ConstructParticle();
-
-  // Construct all baryons
-  G4BaryonConstructor baryons;
-  baryons.ConstructParticle();
-
-  // Construct all ions
-  G4IonConstructor ions;
-  ions.ConstructParticle();
-
-  // Construct all short lived particles
-  G4ShortLivedConstructor shortLived;
-  shortLived.ConstructParticle();
+  
 }
 
-void ESSnusbPhysicsList::ConstructProcess() {}
+void ESSnusbPhysicsList::ConstructProcess(){
+    AddTransportation();
+}
 
 void ESSnusbPhysicsList::SetCuts() {
-  G4VUserPhysicsList::SetCuts(); // TODO set essnusbCuts
+  if (verboseLevel >1){
+    G4cout << "ESSnusbPhysicsList::SetCuts:";
+  }
+  //  " G4VUserPhysicsList::SetCutsWithDefault" method sets
+  //   the default cut value for all particle types
+  SetCutsWithDefault();
 }
 
 } // namespace physicslist
--- framework/core/physicsList/ESSnusbPhysicsList.hpp
+++ framework/core/physicsList/ESSnusbPhysicsList.hpp
@@ -3,13 +3,41 @@
 #ifndef ESSnusbPhysicsList_hpp
 #define ESSnusbPhysicsList_hpp 1
 
-#include "G4VUserPhysicsList.hh"
+#include "G4ios.hh"
+//#include "iomanip.h"
+#include "globals.hh"
+
+#include "G4VModularPhysicsList.hh"
+
+#include "G4ParticleDefinition.hh"
+#include "G4ParticleTypes.hh"
+#include "G4ParticleWithCuts.hh"
+#include "G4ParticleTable.hh"
+#include "G4Material.hh"
+#include "G4MaterialTable.hh"
+#include "G4ProcessManager.hh"
+#include "G4ProcessVector.hh"
+
+#include "G4EmStandardPhysics.hh"
+#include "G4EmLivermorePhysics.hh"
+#include "G4EmPenelopePhysics.hh"
+#include "G4EmLowEPPhysics.hh"
+#include "G4DecayPhysics.hh"
+#include "G4IonPhysics.hh"
+#include "G4EmExtraPhysics.hh"
+#include "G4StoppingPhysics.hh"
+#include "G4OpticalPhysics.hh"
+#include "G4HadronElasticPhysics.hh"
+
+#include "G4UnitsTable.hh"
+#include "G4SystemOfUnits.hh"
+#include "G4LossTableManager.hh"
 
 namespace esbroot {
 namespace esbcore {
 namespace physicslist {
 
-class ESSnusbPhysicsList : public G4VUserPhysicsList {
+class ESSnusbPhysicsList : public G4VModularPhysicsList {
 public:
   ESSnusbPhysicsList();
   virtual ~ESSnusbPhysicsList();
