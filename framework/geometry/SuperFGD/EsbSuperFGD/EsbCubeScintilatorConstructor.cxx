fc4c33657496efba3b8ecd6badbfd8b694833ea2
rigel0586 <rigel0586@gmail.com>
2024-02-01 23:00:01 +0200

    Add material construction without media.geo

--- framework/geometry/SuperFGD/EsbSuperFGD/EsbCubeScintilatorConstructor.cxx
+++ framework/geometry/SuperFGD/EsbSuperFGD/EsbCubeScintilatorConstructor.cxx
@@ -8,6 +8,7 @@
 #include "TGeoBBox.h"
 #include "TGeoCone.h"
 #include "TGeoCompositeShape.h"
+#include "TGeoElement.h"
 
 namespace esbroot {
 namespace geometry {
@@ -46,13 +47,25 @@ TGeoVolume* CubeScintConstructor::Construct()
                                 GetLength()/2 - coatingThickness);
 
   
+  TGeoElementTable *table = gGeoManager->GetElementTable();
 
-  TGeoMedium *tiO2 = gGeoManager->GetMedium(materials::titaniumDioxide);
-  TGeoMedium *c8H8 = gGeoManager->GetMedium(materials::polystyrene);
+  // titanium oxide TiO2 -2  47.867  15.9994  22.  8.  4.26  1  2
+  TGeoMixture *titaniumOxide = new TGeoMixture(materials::titaniumDioxide,2, 4.26);
+  TGeoElement *titanium = table->GetElement(22);
+  TGeoElement *oxygen = table->GetElement(8);
+  titaniumOxide->AddElement(titanium, 1);
+  titaniumOxide->AddElement(oxygen, 2);
+
+  // polystyrene -2 12.01 1.008 6. 1. 1.032 8. 8.
+  TGeoMixture *polystyrene = new TGeoMixture(materials::polystyrene,2, 1.032);
+  TGeoElement *carbon = table->GetElement(6);
+  TGeoElement *hydrogen = table->GetElement(1);
+  polystyrene->AddElement(carbon, 8);
+  polystyrene->AddElement(hydrogen, 8);
 
   TGeoMixture *scintillatorCoating = new TGeoMixture(materials::scintillatorCoating,2, 1.164);
-  scintillatorCoating->AddElement(tiO2->GetMaterial(), 0.15);
-  scintillatorCoating->AddElement(c8H8->GetMaterial(), 0.85);
+  scintillatorCoating->AddElement(titaniumOxide, 0.15);
+  scintillatorCoating->AddElement(polystyrene, 0.85);
   TGeoMedium* coatingMedium = new TGeoMedium("coatingMedium", materials::GetNextIndex(), scintillatorCoating);
 
   //=======================================================================================
@@ -80,10 +93,22 @@ TGeoVolume* CubeScintConstructor::Construct()
   TGeoCompositeShape* fiberXShapeCoat = new TGeoCompositeShape("fiberXShapeCoat","FX - fiberShapeX");
 
   // Create fiber coating
-  TGeoVolume* fiberXCoatVolume = new TGeoVolume("fiberXCoatVolume",fiberXShapeCoat, gGeoManager->GetMedium(materials::fiberCladding));
+  //   fiberCladding       -3  12.0107  1.008 15.9994  6.  1.  8.	1.19  5	 8	2
+  TGeoMixture *fiberCladding = new TGeoMixture(materials::fiberCladding,3, 1.19);
+  fiberCladding->AddElement(carbon, 5);
+  fiberCladding->AddElement(hydrogen, 8);
+  fiberCladding->AddElement(oxygen, 2);
+  TGeoMedium* fiberCladdingMedium = new TGeoMedium("fiberCladdingMedium", materials::GetNextIndex(), fiberCladding);
+  TGeoVolume* fiberXCoatVolume = new TGeoVolume("fiberXCoatVolume",fiberXShapeCoat, fiberCladdingMedium);
 
   // Create fiber core volume
-  TGeoVolume* fiberXVolume = new TGeoVolume("fiberXVolume",fiberShapeX, gGeoManager->GetMedium(materials::fiberCore));
+  // fiberCore        	-2  12.0107  1.008  6.  1.  1.05  9  10
+  TGeoMixture *fiberCore= new TGeoMixture(materials::fiberCore ,3, 1.19);
+  fiberCladding->AddElement(carbon, 5);
+  fiberCladding->AddElement(hydrogen, 8);
+  fiberCladding->AddElement(oxygen, 2);
+  TGeoMedium* fiberCoreMedium = new TGeoMedium("fiberCoreMedium", materials::GetNextIndex(), fiberCore);
+  TGeoVolume* fiberXVolume = new TGeoVolume("fiberXVolume",fiberShapeX, fiberCoreMedium);
 
   // Place the fiber core inside its coating
   fiberXCoatVolume->AddNode(fiberXVolume, 1 /* One copy*/);
@@ -116,10 +141,10 @@ TGeoVolume* CubeScintConstructor::Construct()
   TGeoCompositeShape* fiberYShapeCoat = new TGeoCompositeShape("fiberYShapeCoat","FY - fiberShapeY");
 
   // Create fiber coating
-  TGeoVolume* fiberYCoatVolume = new TGeoVolume("fiberYCoatVolume",fiberYShapeCoat, gGeoManager->GetMedium(materials::fiberCladding));
+  TGeoVolume* fiberYCoatVolume = new TGeoVolume("fiberYCoatVolume",fiberYShapeCoat, fiberCladdingMedium);
 
   // Create fiber core volume
-  TGeoVolume* fiberYVolume = new TGeoVolume("fiberYVolume",fiberShapeY, gGeoManager->GetMedium(materials::fiberCore));
+  TGeoVolume* fiberYVolume = new TGeoVolume("fiberYVolume",fiberShapeY, fiberCoreMedium);
 
   // Place the fiber core inside its coating
   fiberYCoatVolume->AddNode(fiberYVolume, 1 /* One copy*/);
@@ -153,10 +178,10 @@ TGeoVolume* CubeScintConstructor::Construct()
   TGeoCompositeShape* fiberZShapeCoat = new TGeoCompositeShape("fiberZShapeCoat","FZ - fiberShapeZ");
 
   // Create fiber coating
-  TGeoVolume* fiberZCoatVolume = new TGeoVolume("fiberZCoatVolume",fiberZShapeCoat, gGeoManager->GetMedium(materials::fiberCladding));
+  TGeoVolume* fiberZCoatVolume = new TGeoVolume("fiberZCoatVolume",fiberZShapeCoat, fiberCladdingMedium);
 
   // Create fiber core volume
-  TGeoVolume* fiberZVolume = new TGeoVolume("fiberZVolume",fiberShapeZ, gGeoManager->GetMedium(materials::fiberCore));
+  TGeoVolume* fiberZVolume = new TGeoVolume("fiberZVolume",fiberShapeZ, fiberCoreMedium);
 
   // Place the fiber core inside its coating
   fiberZCoatVolume->AddNode(fiberZVolume, 1 /* One copy*/);
@@ -168,12 +193,20 @@ TGeoVolume* CubeScintConstructor::Construct()
   TGeoCompositeShape* cubeComp = new TGeoCompositeShape("cubeComp","Cube - FX:locationX - FY:locationY - FY:locationZ");
 
   // Define Scintilator mix
-  TGeoMedium *scnt = gGeoManager->GetMedium(materials::scintillator);
-  TGeoMedium *prt = gGeoManager->GetMedium(materials::paraterphnyl);
+  // scintillator		-2 12.01 1.008 6. 1. 1.050 8. 8.
+  TGeoMixture *scnt_mix = new TGeoMixture(materials::scintillator,2, 1.050);
+  scnt_mix->AddElement(carbon, 8);
+  scnt_mix->AddElement(hydrogen, 8);
+
+  // paraterphnyl		-2 12.01 1.008 6. 1. 1.240 18. 14.
+  TGeoMixture *prt_mix = new TGeoMixture(materials::paraterphnyl,2, 1.240);
+  prt_mix->AddElement(carbon, 18);
+  prt_mix->AddElement(hydrogen, 14);
+
 
   TGeoMixture *scintillatorMixMat = new TGeoMixture(materials::scintilatorMix,2, 1.050); 
-  scintillatorMixMat->AddElement(scnt->GetMaterial(), 0.985);
-  scintillatorMixMat->AddElement(prt->GetMaterial(), 0.015);
+  scintillatorMixMat->AddElement(scnt_mix, 0.985);
+  scintillatorMixMat->AddElement(prt_mix, 0.015);
 
   TGeoMedium* scintillatorMixMedium = new TGeoMedium("scintillatorMixMat", materials::GetNextIndex(), scintillatorMixMat);
 
@@ -186,7 +219,14 @@ TGeoVolume* CubeScintConstructor::Construct()
   else
   {
       // Create the mother cube volume 
-      TGeoMedium *vacuum = gGeoManager->GetMedium(materials::vacuum);
+      // vacuum             3  14.01  16.  39.95  7.  8.  18.  1.205e-14  .755  .231  .014
+      TGeoMixture *vacuumMix = new TGeoMixture(materials::vacuum,3, 1.205e-14);
+      TGeoElement *nitrogen = table->GetElement(7);
+      TGeoElement *argon = table->GetElement(18);
+      vacuumMix->AddElement(nitrogen, 0.755);
+      vacuumMix->AddElement(oxygen, 0.231);
+      vacuumMix->AddElement(argon, 0.014);
+      TGeoMedium* vacuum = new TGeoMedium("vacuumMedium", materials::GetNextIndex(), vacuumMix);
       TGeoVolume* cubeWithCoatingVolume = new TGeoVolume(fgdnames::cubeName,cubeWithCoating, vacuum);
 
       // Place the coating
