6768d59cb51f0c957aade57824a6bc962deb0ec4
rigel0586 <rigel0586@gmail.com>
2024-02-14 23:18:44 +0200

    Add check_build for geant4 tars

--- check_build.sh
+++ check_build.sh
@@ -44,6 +44,42 @@ cd ${fw_directory}
 geant4_check_lib=${fw_directory}/geant4/install/lib/libG4clhep.so
 check_lib_exists ${geant4_check_lib}
 
+g4ndl47_var=${fw_directory}/framework/workspace/geant4/download/G4NDL4.7/README
+check_lib_exists ${g4ndl47_var}
+
+G4EMLOW_var=${fw_directory}/framework/workspace/geant4/download/G4EMLOW8.5/README
+check_lib_exists ${G4EMLOW_var}
+
+G4PhotonEvaporation_var=${fw_directory}/framework/workspace/geant4/download/PhotonEvaporation5.7/z1.a3
+check_lib_exists ${G4PhotonEvaporation_var}
+
+G4RadioactiveDecay_var=${fw_directory}/framework/workspace/geant4/download/RadioactiveDecay5.6/README_RDM
+check_lib_exists ${G4RadioactiveDecay_var}
+
+G4PARTICLEXS_var=${fw_directory}/framework/workspace/geant4/download/G4PARTICLEXS4.0/README
+check_lib_exists ${G4PARTICLEXS_var}
+
+G4PII_var=${fw_directory}/framework/workspace/geant4/download/G4PII1.3/documentation.pdf
+check_lib_exists ${G4PII_var}
+
+G4RealSurface_var=${fw_directory}/framework/workspace/geant4/download/RealSurface2.2/README
+check_lib_exists ${G4RealSurface_var}
+
+G4SAIDDATA_var=${fw_directory}/framework/workspace/geant4/download/G4SAIDDATA2.0/README_pipiN
+check_lib_exists ${G4SAIDDATA_var}
+
+G4ABLA_var=${fw_directory}/framework/workspace/geant4/download/G4ABLA3.3/README
+check_lib_exists ${G4ABLA_var}
+
+G4INCL_var=${fw_directory}/framework/workspace/geant4/download/G4INCL1.2/README
+check_lib_exists ${G4INCL_var}
+
+G4ENSDFSTATE_var=${fw_directory}/framework/workspace/geant4/download/G4ENSDFSTATE2.3/README
+check_lib_exists ${G4ENSDFSTATE_var}
+
+G4TENDL_var=${fw_directory}/framework/workspace/geant4/download/G4TENDL1.4/README
+check_lib_exists ${G4TENDL}
+
 cd ${fw_directory}
 
 # Genfit
