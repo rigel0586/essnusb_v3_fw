bd480a191a37fa55e4979d34bc7ab8283279abb0
rigel0586 <rigel0586@gmail.com>
2024-02-17 02:26:02 +0200

    Add mppc display pics

---
+++ framework/scripts/superfgd/simulate_3_generate_pics.C
@@ -0,0 +1,42 @@
+/*
+
+  Macro that shows how to run the WC digitization on an existing simulation
+  (e.g., produced with ess_sim.C).
+
+  Based on the example in the presentation from
+  Konstantin Gertsenberger
+  
+  .L ess_dig_gen_pics.C
+  ess_dig_gen_pics()
+  
+*/
+
+void simulate_3_generate_pics(Int_t nEvents = 4)
+{
+  using namespace esbroot;
+
+  std::stringstream ssInput;
+  ssInput << gSystem->Getenv("WORKSPACE_DIR");
+  ssInput << "/simulation/digitization_output.root";
+  std::string inputFile = ssInput.str();
+  core::task::EsbTaskManager* fRun = new core::task::EsbTaskManager(inputFile 
+          , geometry::superfgd::DP::FGD_DETECTOR_NAME.c_str() 
+          , geometry::superfgd::DP::FGD_HIT.c_str());
+  
+  fRun->setNumberOfEvents(nEvents);   
+
+  std::stringstream ssOut;
+  ssOut << gSystem->Getenv("WORKSPACE_DIR");
+  ssOut << "/simulation/pics_output.root";
+  std::string outputFile = ssOut.str();
+  fRun->setOutputFile(outputFile);
+ 
+  // Set Tasks 
+  std::stringstream ssConf;
+  ssConf << gSystem->Getenv("ESB_BASE_DIR");
+  ssConf << "/geometry/SuperFGD/EsbSuperFGD/EsbConfig/fgdconfig";
+  std::string fgdConfig = ssConf.str();
+  core::task::ITask* mppcDisplayTask = new digitizer::superfgd::FgdMppcDisplay("Granular Task", fgdConfig.c_str(),0,0,0);
+  fRun->addTask(mppcDisplayTask);   
+  fRun->run();
+}
