44092631696125f2e4dafdab4efccfb63e318e0c
rigel0586 <rigel0586@gmail.com>
2025-02-27 00:28:19 +0200

    Upgrade for linux mint 22.1

--- build.sh
+++ build.sh
@@ -211,20 +211,16 @@ export GENIE=${fw_directory}/genie/build/genie
 export ROOTSYS=${fw_directory}/cern_root/install
 export PATH=$PATH:${fw_directory}/cern_root/install/bin
 export PYTHIA6=${dependencies_dir}/genie/pythia6/v6_428/lib/
-export LHAPDF5_INC=${dependencies_dir}/genie/lhapdf-5.9.1/install/include/
-export LHAPDF5_LIB=${dependencies_dir}/genie/lhapdf-5.9.1/install/lib/ 
-export LHAPDF6_INC=${dependencies_dir}/genie/LHAPDF-6.5.4/install/include/
-export LHAPDF6_LIB=${dependencies_dir}/genie/LHAPDF-6.5.4/install/lib/ 
 export GEANT4_INC=${fw_directory}/geant4/install/include/
 export GEANT4_LIB=${fw_directory}/geant4/install/lib/ 
 
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${fw_directory}/geant4/install/lib/:${dependencies_dir}/genie/lhapdf-5.9.1/install/lib/:${dependencies_dir}/genie/log4cpp/install/lib/:${dependencies_dir}/geant4/2.4.6.4/install/lib/:${dependencies_dir}/genie/log4cpp/install/lib/
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${fw_directory}/geant4/install/lib/:${dependencies_dir}/genie/log4cpp/install/lib/:${dependencies_dir}/geant4/2.4.6.4/install/lib/:${dependencies_dir}/genie/log4cpp/install/lib/
 
 ./configure --prefix=${fw_directory}/genie/install \
 --disable-profiler \
 --disable-validation-tools \
---disable-cernlib \
---enable-lhapdf \
+--enable-cernlib \
+--disable-lhapdf \
 --enable-flux-drivers \
 --enable-geom-drivers \
 --disable-doxygen \
@@ -242,8 +238,6 @@ export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${fw_directory}/geant4/install/lib/:${
 --with-pythia6-lib=${dependencies_dir}/genie/pythia6/v6_428/lib/  \
 --with-log4cpp-inc=${dependencies_dir}/genie/log4cpp/install/include/  \
 --with-log4cpp-lib=${dependencies_dir}/genie/log4cpp/install/lib/  \
---lhapdf5-inc=${dependencies_dir}/genie/lhapdf-5.9.1/install/include/  \
---lhapdf5-lib=${dependencies_dir}/genie/lhapdf-5.9.1/install/lib/ \
 --geant4-inc=${fw_directory}/geant4/install/include/ \
 --geant4-lib=${fw_directory}/geant4/install/lib/ \
 
--- env_setup.sh
+++ env_setup.sh
@@ -22,10 +22,6 @@ export GENIE_install=${fw_directory}/genie/install
 export ROOTSYS=${fw_directory}/cern_root/install
 export PATH=$PATH:${fw_directory}/cern_root/install/bin
 export PYTHIA6=${dependencies_dir}/genie/pythia6/v6_428/lib/
-export LHAPDF5_INC=${dependencies_dir}/genie/lhapdf-5.9.1/install/include/
-export LHAPDF5_LIB=${dependencies_dir}/genie/lhapdf-5.9.1/install/lib/ 
-export LHAPDF6_INC=${dependencies_dir}/genie/LHAPDF-6.5.4/install/include/
-export LHAPDF6_LIB=${dependencies_dir}/genie/LHAPDF-6.5.4/install/lib/ 
 export GEANT4_INC=${fw_directory}/geant4/install/include/
 export GEANT4_LIB=${fw_directory}/geant4/install/lib/ 
 export GENFIT_LIB=${fw_directory}/genfit/install/lib/ 
@@ -33,7 +29,7 @@ export VGM_LIB=${fw_directory}/vgm/install/lib/
 export PYTHONPATH=$ROOTSYS/lib:$ROOTSYS/geom/gdml
 
 framework_build_dir=${fw_directory}/framework/build
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${fw_directory}/geant4/install/lib/:${dependencies_dir}/genie/lhapdf-5.9.1/install/lib/:${dependencies_dir}/genie/log4cpp/install/lib/:${dependencies_dir}/geant4/2.4.6.4/install/lib/:${dependencies_dir}/genie/log4cpp/install/lib/:${LHAPDF6_LIB}:${LHAPDF5_LIB}/.libs:/usr/local/lib:${GENIE_install}/lib:${ROOTSYS}/lib:${PYTHIA6}:${framework_build_dir}/utility/:${framework_build_dir}/generators/:${framework_build_dir}/data/:${framework_build_dir}/geometry/:${framework_build_dir}/core/:${framework_build_dir}/digitizer/:${framework_build_dir}/reconstruction/:${framework_build_dir}/eve/:${fw_directory}/pathfinder/install/lib:${dependencies_dir}/geant4/xerces-c-3.2.5/install/lib:${fw_directory}/clhep/lib
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${fw_directory}/geant4/install/lib/:${dependencies_dir}/genie/log4cpp/install/lib/:${dependencies_dir}/geant4/2.4.6.4/install/lib/:${dependencies_dir}/genie/log4cpp/install/lib/:/usr/local/lib:${GENIE_install}/lib:${ROOTSYS}/lib:${PYTHIA6}:${framework_build_dir}/utility/:${framework_build_dir}/generators/:${framework_build_dir}/data/:${framework_build_dir}/geometry/:${framework_build_dir}/core/:${framework_build_dir}/digitizer/:${framework_build_dir}/reconstruction/:${framework_build_dir}/eve/:${fw_directory}/pathfinder/install/lib:${dependencies_dir}/geant4/xerces-c-3.2.5/install/lib:${fw_directory}/clhep/lib
 
 export PATH=$PATH:${ROOTSYS}/bin:${GENIE_install}/bin 
 
