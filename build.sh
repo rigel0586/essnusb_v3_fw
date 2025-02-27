d2c43a6152001ba4a43f3ca92e76e3b5b192f460
rigel0586 <rigel0586@gmail.com>
2025-02-27 02:28:04 +0200

    Update build.sh
    Pythia6 is not longer supported in v6.32.10

--- build.sh
+++ build.sh
@@ -43,9 +43,7 @@ cd cern_root
 mkdir build
 mkdir install
 cd build
-pythia6_inc=${dependencies_dir}/genie/pythia6/v6_428/inc/
-pythia6_lib=${dependencies_dir}/genie/pythia6/v6_428/lib/libPythia6.so
-cmake -S ${root_dir} -B . -DCMAKE_INSTALL_PREFIX=${fw_directory}/cern_root/install -Dmathmore=ON -Dtmva-sofie=ON -Dopengl=ON -Dpythia6=pythia6 -DPYTHIA6_INCLUDE_DIR=${pythia6_inc} -DPYTHIA6_LIBRARY=${pythia6_lib}
+cmake -S ${root_dir} -B . -DCMAKE_INSTALL_PREFIX=${fw_directory}/cern_root/install -Dmathmore=ON -Dtmva-sofie=ON -Dopengl=ON
 cmake --build . 
 cmake --install .
 
