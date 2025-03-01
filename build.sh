ca708f6cf59304679df91c97722afe9440bebc2e
rigel0586 <rigel0586@gmail.com>
2025-03-02 00:10:13 +0200

    Add builtin vdt as flag ON to cern roo build

--- build.sh
+++ build.sh
@@ -45,7 +45,7 @@ mkdir install
 cd build
 pythia6_inc=${dependencies_dir}/genie/pythia6/v6_428/inc/
 pythia6_lib=${dependencies_dir}/genie/pythia6/v6_428/lib/libPythia6.so
-cmake -S ${root_dir} -B . -DCMAKE_INSTALL_PREFIX=${fw_directory}/cern_root/install -Dmathmore=ON -Dtmva-sofie=ON -Dopengl=ON -Dpythia6=pythia6 -DPYTHIA6_INCLUDE_DIR=${pythia6_inc} -DPYTHIA6_LIBRARY=${pythia6_lib}
+cmake -S ${root_dir} -B . -DCMAKE_INSTALL_PREFIX=${fw_directory}/cern_root/install -Dmathmore=ON -Dtmva-sofie=ON -Dopengl=ON -Dpythia6=pythia6 -DPYTHIA6_INCLUDE_DIR=${pythia6_inc} -DPYTHIA6_LIBRARY=${pythia6_lib} -Dbuiltin_vdt=ON
 cmake --build . 
 cmake --install .
 
